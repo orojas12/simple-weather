651374dec513e4a5a0f9ee6aa8e0b8723c8d4a5b
Oscar <orojas12@outlook.com>
2023-02-01 22:39:59 -0700

    add base Search component

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,16 @@ import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./App.css";
 
-import Combobox from "./components/Combobox/Combobox";
-import search from "./icons/search.svg";
-import location from "./icons/location.svg";
+import { Search } from "components";
 
 export default function App() {
   const [useCelsius, setUseCelsius] = useState(false);
-
-  const searchIcon = (
-    <img src={search} style={{ height: "100%", width: "100%" }} />
-  );
-  const locationIcon = (
-    <img src={location} style={{ height: "100%", width: "100%" }} />
-  );
 
   return (
     <div className="App">
       <header>
         <div id="controls">
-          <Combobox<number>
-            id="ComboBox1"
-            label="Location"
-            items={[
-              { id: "1", text: "test1", data: 1 },
-              { id: "2", text: "test2", data: 2 },
-            ]}
-            select={(num) => {
-              console.log(num);
-            }}
-            inputIcon={searchIcon}
-            itemIcon={locationIcon}
-          />
+          <Search setLocation={() => {}} />
         </div>
         <nav></nav>
       </header>
