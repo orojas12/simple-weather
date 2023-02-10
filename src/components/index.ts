930aec8b158e14080ae5f735e86585b34d61ee6b
Oscar <orojas12@outlook.com>
2023-02-09 17:09:59 -0700

    create Dropdown component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import Combobox from "./Combobox/Combobox";
 import Search from "./Search/Search";
+import Dropdown from "./Dropdown/Dropdown";
 
-export { Combobox, Search };
+export { Combobox, Search, Dropdown };
