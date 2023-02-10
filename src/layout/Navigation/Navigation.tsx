988c785a7b80430fe4cdb5996af803a36396e19d
Oscar <orojas12@outlook.com>
2023-02-09 17:10:23 -0700

    delete unused component

--- src/layout/Navigation/Navigation.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./navigation.css";
-
-export interface NavigationProps {}
-
-export default function Navigation(props: NavigationProps) {
-  return <nav className="navigation"></nav>;
-}
