e435169afc45e3cbbea88649dd74bbe1ecffb2c8
Oscar <orojas12@outlook.com>
2023-02-13 10:20:25 -0700

    rebuild dropdown component to use a friendlier api

--- client/src/components/Dropdown/Dropdown.tsx
+++ client/src/components/Dropdown/Dropdown.tsx
@@ -1,61 +1,89 @@
-import React, { useState } from "react";
+import React, { useState, createContext, useContext } from "react";
 import { ArrowDownIcon } from "icons";
 import "./dropdown.css";
 
 export interface DropdownProps {
-  align: "start" | "end";
-  btnText: React.ReactNode;
   className?: string;
   children?: React.ReactNode;
 }
 
-export default function Dropdown({
-  align,
-  children,
-  btnText,
-  className,
-}: DropdownProps) {
+interface IOpenContext {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const DropdownContext = createContext<IOpenContext | null>(null);
+
+export default function Dropdown({ children, className }: DropdownProps) {
   const [open, setOpen] = useState(false);
-
-  const style = {
-    left: align === "start" ? 0 : undefined,
-    right: align === "end" ? 0 : undefined,
-  };
-
-  const btn =
-    typeof btnText === "string" ? <button>{btnText}</button> : btnText;
 
   return (
     <div className={`dropdown ${className || ""}`}>
-      <div
-        role="button"
-        className="dropdown__button"
-        onClick={() => {
-          setOpen((prevState) => !prevState);
-        }}
-      >
-        {btn}
-        <ArrowDownIcon
-          className={`dropdown__icon ${open && "dropdown__icon--active"}`}
-        />
-      </div>
-      {open ? (
-        <>
-          <div
-            className="dropdown__backdrop"
-            onClick={() => setOpen(false)}
-          ></div>
-          <div className="dropdown__content-wrapper">
-            <div
-              className="dropdown__content"
-              style={style}
-              onClick={() => setOpen(false)}
-            >
-              {children}
-            </div>
-          </div>
-        </>
-      ) : undefined}
+      <DropdownContext.Provider value={{ open, setOpen }}>
+        {children}
+      </DropdownContext.Provider>
     </div>
   );
 }
+
+function Toggle({ children }: { children: React.ReactNode }) {
+  const ctx = useContext(DropdownContext);
+  return (
+    <button
+      className={`dropdown__button ${
+        ctx?.open ? "dropdown__button--active" : ""
+      }`}
+      onClick={() => ctx?.setOpen((prevState) => !prevState)}
+    >
+      {children}
+    </button>
+  );
+}
+
+function ToggleIcon({ children }: { children?: React.ReactNode }) {
+  const ctx = useContext(DropdownContext);
+  return children ? (
+    <div>{children}</div>
+  ) : (
+    <ArrowDownIcon
+      className={`dropdown__icon ${ctx?.open && "dropdown__icon--active"}`}
+    />
+  );
+}
+
+function Item({ children }: { children: React.ReactNode }) {
+  const ctx = useContext(DropdownContext);
+  return (
+    <li className="dropdown__item" onClick={() => ctx?.setOpen(false)}>
+      {children}
+    </li>
+  );
+}
+
+function List({
+  align = "start",
+  children,
+}: {
+  align?: "start" | "end";
+  children: React.ReactNode;
+}) {
+  const ctx = useContext(DropdownContext);
+  return ctx?.open ? (
+    <>
+      <div
+        className="dropdown__backdrop"
+        onClick={() => ctx?.setOpen(false)}
+      ></div>
+      <div
+        className={`dropdown__list-wrapper dropdown__list-wrapper--${align}`}
+      >
+        <ul className={"dropdown__list"}>{children}</ul>
+      </div>
+    </>
+  ) : null;
+}
+
+Dropdown.Toggle = Toggle;
+Dropdown.ToggleIcon = ToggleIcon;
+Dropdown.Item = Item;
+Dropdown.List = List;
