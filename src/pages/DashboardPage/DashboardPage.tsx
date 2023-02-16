6eb7ba5f333b36ca484af91bbc8bfefacfb82e4d
Oscar <orojas12@outlook.com>
2023-02-16 12:01:31 -0700

    add clock

--- src/pages/DashboardPage/DashboardPage.tsx
+++ src/pages/DashboardPage/DashboardPage.tsx
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Dropdown } from "components";
 import { LocationIcon } from "icons";
+import { LocationContext } from "hooks";
+import Clock from "./Clock";
 import "./dashboard.css";
 
 export default function DashboardPage() {
+  const location = useContext(LocationContext);
+
   return (
     <article className="dashboard">
       <header>
-        <div className="dashboard__clock"></div>
-        <Dropdown
-          className="dashboard__dropdown"
-          align={"start"}
-          btnText={
-            <div>
-              <LocationIcon className="dashboard__dropdown-icon" />
-              <span>Location</span>
-            </div>
-          }
-        >
-          <div className="">Location 1</div>
-          <div>Location 2</div>
-          <div>Location 3</div>
+        <Clock />
+        <Dropdown id="dropdown1" size="small">
+          <Dropdown.Toggle>
+            <LocationIcon className="dashboard__location-icon" />
+            {location?.place?.description}
+            <Dropdown.ToggleIcon />
+          </Dropdown.Toggle>
+          <Dropdown.Menu
+            align="end"
+            items={[
+              {
+                content: "Location 123",
+                action: () => console.log("Location 123"),
+              },
+              {
+                content: <div>Location 2</div>,
+                action: () => console.log("Location 2"),
+              },
+              {
+                content: "Location 3",
+                action: () => console.log("Location 3"),
+              },
+            ]}
+          />
         </Dropdown>
       </header>
     </article>
