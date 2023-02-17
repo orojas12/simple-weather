99016d395c3b9c21e2fa68fc765956c640994a1c
Oscar <orojas12@outlook.com>
2023-02-17 12:41:32 -0700

    move weather module into hooks

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,4 +1,5 @@
 import useLocation, { Location, Coords, LocationContext } from "./useLocation";
+import useWeather from "./useWeather";
 
 export type { Location, Coords };
-export { useLocation, LocationContext };
+export { useLocation, LocationContext, useWeather };
