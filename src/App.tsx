2587d52c99d008e9cd2acbc989203260832858dc
Oscar <orojas12@outlook.com>
2023-02-17 20:52:42 -0700

    add weather cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import React, { createContext } from "react";
 import { Outlet } from "react-router-dom";
-import { useLocation, LocationContext } from "hooks";
+import {
+  useLocation,
+  LocationContext,
+  useWeather,
+  WeatherContext,
+} from "hooks";
 import { Navbar } from "layout";
 import "./app.css";
 
 export default function App() {
   const location = useLocation();
+  const { weather, updateWeather, setCoords } = useWeather(33.44, -94.04);
+
+  location.place = {
+    placeId: "1",
+    description: "El Paso, TX, USA",
+    mainText: "El Paso",
+    secondaryText: "TX, USA",
+  };
 
   return (
     <div className="App">
       <Navbar />
       <main>
         <LocationContext.Provider value={location}>
-          <Outlet />
+          <WeatherContext.Provider value={weather}>
+            <Outlet />
+          </WeatherContext.Provider>
         </LocationContext.Provider>
       </main>
     </div>
