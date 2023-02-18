6c67c85e8fbce29bb0a1e7d65afc6dfd4a8603e6
Oscar <orojas12@outlook.com>
2023-02-17 20:51:20 -0700

    add weather exports

--- src/hooks/useWeather/index.ts
+++ src/hooks/useWeather/index.ts
@@ -1,3 +1,5 @@
-import useWeather from "./useWeather";
+import useWeather, { WeatherContext, WeatherData } from "./useWeather";
 
+export type { WeatherData };
+export { WeatherContext };
 export default useWeather;
