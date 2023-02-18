b30c5d9fb421efb3fcb1e9c3bbb0faf55cbc5323
Oscar <orojas12@outlook.com>
2023-02-17 23:35:28 -0700

    fix merge conflict

--- client/src/icons/ui/index.ts
+++ client/src/icons/ui/index.ts
@@ -1,19 +1,3 @@
-import LogoIcon from "./LogoIcon";
-import GearIcon from "./GearIcon";
-import GpsIcon from "./GpsIcon";
-import HomeIcon from "./HomeIcon";
-import LocationIcon from "./LocationIcon";
-import SearchIcon from "./SearchIcon";
-import MenuIcon from "./MenuIcon";
-import ArrowDownIcon from "./ArrowDownIcon";
+import IconProps from "../IconProps";
 
-export {
-  LogoIcon,
-  GearIcon,
-  GpsIcon,
-  HomeIcon,
-  LocationIcon,
-  SearchIcon,
-  MenuIcon,
-  ArrowDownIcon,
-};
+export type { IconProps };
