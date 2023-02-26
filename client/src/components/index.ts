a3756112bfd8fd7286da3f0b5f71aa4b2b18a90a
Oscar <orojas12@outlook.com>
2023-02-26 14:15:44 -0700

    add Progress component

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -4,5 +4,6 @@ import Dropdown from "./Dropdown/Dropdown";
 import Card from "./Card/Card";
 import Navbar from "./Navbar/Navbar";
 import Accordian from "./Accordian/Accordian";
+import Progress from "./Progress/Progress";
 
-export { Combobox, Search, Dropdown, Card, Navbar, Accordian };
+export { Combobox, Search, Dropdown, Card, Navbar, Accordian, Progress };
