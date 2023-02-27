6b81b659c04ac0a15e53da4316eb23af50b5684a
Oscar <orojas12@outlook.com>
2023-02-27 11:13:32 -0700

    add weather route

--- server/src/routes/weather.ts
+++ server/src/routes/weather.ts
@@ -1,9 +1,30 @@
 import { Router } from "express";
+import fetch from "node-fetch";
 
+const { WEATHER_API_KEY } = process.env;
 const weatherRouter = Router();
 
-weatherRouter.get("/", (req, res) => {
-  res.send("Weather");
+weatherRouter.get("/", async (req, res) => {
+  if (!(req.query.lat && req.query.lng)) {
+    res
+      .status(400)
+      .json({ message: "Required query parameters not specified" });
+  }
+
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${req.query.lat}&lon=${req.query.lng}&exclude=minutely&appid=${WEATHER_API_KEY}`
+    );
+    const data = await response.json();
+
+    if (!response.ok) {
+      res.status(response.status).json(data);
+    }
+
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 export default weatherRouter;
