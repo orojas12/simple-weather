4e646ed6117e8408b330ddcb0f9d59b99f63b75a
Oscar <orojas12@outlook.com>
2023-02-27 11:54:57 -0700

    add access logs

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,22 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+import fs from "fs";
+import path from "path";
 import express from "express";
+import morgan from "morgan";
 import { weatherRouter } from "./routes";
+
+const accessLogStream = fs.createWriteStream(
+  path.resolve(__dirname, "../logs/access.log"),
+  { flags: "a" }
+);
 
 const app = express();
 const host = process.env.HOST || "127.0.0.1";
 const port = parseInt(process.env.PORT || "8080");
 
+app.use(morgan("short", { stream: accessLogStream }));
 app.use("/weather", weatherRouter);
 
 app.listen(port, host, () => {
