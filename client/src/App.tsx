ba249f0ee772449ffd2b1d4224d88e24781b00ef
Oscar <orojas12@outlook.com>
2023-03-02 16:25:29 -0700

    add Toast component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
-import React, { createContext, useEffect } from "react";
+import React, { useState, createContext, useEffect, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import { useLocation, useWeather, WeatherContext } from "hooks";
 import { LocationContext } from "hooks/useLocation";
-import { Navbar } from "components";
+import { Navbar, Toast, ToastContext, ToastProps } from "components";
 import "./app.css";
 
 export default function App() {
@@ -11,6 +11,10 @@ export default function App() {
     location.data.activeLocation.lat,
     location.data.activeLocation.lat
   );
+  const [toast, setToast] = useState<ToastProps | null>(null);
+  const [toastActive, setToastActive] = useState(false);
+  const toastTimeout = useRef<NodeJS.Timeout | null>(null);
+  const toastActiveTimeout = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     setCoords({
@@ -19,16 +23,46 @@ export default function App() {
     });
   }, [location.data.activeLocation.placeId]);
 
+  useEffect(() => {
+    if (toast) {
+      setToastActive(true);
+      toastTimeout.current = setTimeout(() => {
+        setToast(null);
+      }, 8000);
+    }
+
+    return () => clearTimeout(toastTimeout.current || undefined);
+  }, [toast]);
+
+  useEffect(() => {
+    if (toastActive) {
+      toastActiveTimeout.current = setTimeout(() => {
+        setToastActive(false);
+      }, 6000);
+    }
+
+    return () => clearTimeout(toastActiveTimeout.current || undefined);
+  }, [toastActive]);
+
   return (
     <div className="App">
       <Navbar />
       <main>
         <LocationContext.Provider value={location}>
           <WeatherContext.Provider value={weather}>
-            <Outlet />
+            <ToastContext.Provider value={{ setToast }}>
+              <Outlet />
+            </ToastContext.Provider>
           </WeatherContext.Provider>
         </LocationContext.Provider>
       </main>
+      <Toast
+        type={toast?.type}
+        msg={toast?.msg}
+        active={toastActive}
+        display={Boolean(toast)}
+        onClick={() => setToastActive(false)}
+      />
     </div>
   );
 }
---
+++ client/src/components/Toast/Toast.tsx
@@ -0,0 +1,41 @@
+import React, { createContext } from "react";
+import { Card } from "components";
+import { CheckCircleIcon, CloseCircleIcon } from "icons/ui";
+import "./toast.css";
+
+export interface ToastProps {
+  type?: "success" | "alert";
+  msg?: string;
+  active?: boolean;
+  display?: boolean;
+  onClick?: () => void;
+}
+
+interface IToastContext {
+  setToast: React.Dispatch<React.SetStateAction<ToastProps | null>>;
+}
+
+export const ToastContext = createContext<IToastContext | null>(null);
+
+export default function Toast({
+  type = "success",
+  msg = "Task failed successfully.",
+  active = true,
+  display = true,
+  onClick,
+}: ToastProps) {
+  return (
+    <div className="toast__wrapper" onClick={onClick}>
+      <Card
+        className={`toast toast--${type} ${!display ? "toast--hidden" : ""} ${
+          active ? "toast--active" : ""
+        }`}
+      >
+        <Card.Content className="flex align-center gap-1">
+          {type === "success" ? <CheckCircleIcon /> : <CloseCircleIcon />}
+          {msg}
+        </Card.Content>
+      </Card>
+    </div>
+  );
+}
