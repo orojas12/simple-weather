6d0e653952e4743a730c4329c5e36545a02077cd
Oscar <orojas12@outlook.com>
2023-03-02 18:23:16 -0700

    add geocode route

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import express, { Router } from "express";
-import { weatherRouter, placesRouter } from "./routes";
+import { weatherRouter, placesRouter, geocodeRouter } from "./routes";
 import { errorLogger, accessLogger } from "./middleware";
 
 const app = express();
@@ -11,7 +11,7 @@ const port = parseInt(process.env.PORT || "8080");
 
 const router = Router();
 
-router.use("/api", [weatherRouter, placesRouter]);
+router.use("/api", weatherRouter, placesRouter, geocodeRouter);
 
 app.use(errorLogger, accessLogger);
 app.use(router);
