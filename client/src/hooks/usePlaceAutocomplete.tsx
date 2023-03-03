5ef213db53c71349b277a45b81472b5cea5df94a
Oscar <orojas12@outlook.com>
2023-03-02 18:22:30 -0700

    remove test data

--- client/src/hooks/usePlaceAutocomplete.tsx
+++ client/src/hooks/usePlaceAutocomplete.tsx
@@ -5,39 +5,6 @@ export interface Place {
   secondaryText: string;
 }
 
-const fakePredictions: Place[] = [
-  {
-    description: "Paris, France",
-    placeId: "ChIJD7fiBh9u5kcRYJSMaMOCCwQ",
-    mainText: "Paris",
-    secondaryText: "France",
-  },
-  {
-    description: "Paris, TX, USA",
-    placeId: "ChIJmysnFgZYSoYRSfPTL2YJuck",
-    mainText: "Paris",
-    secondaryText: "TX, USA",
-  },
-  {
-    description: "Paris, TN, USA",
-    placeId: "ChIJ4zHP-Sije4gRBDEsVxunOWg",
-    mainText: "Paris",
-    secondaryText: "TN, USA",
-  },
-  {
-    description: "Paris, Brant, ON, Canada",
-    placeId: "ChIJsamfQbVtLIgR-X18G75Hyi0",
-    mainText: "Paris",
-    secondaryText: "Brant, ON, Canada",
-  },
-  {
-    description: "Paris, KY, USA",
-    placeId: "ChIJsU7_xMfKQ4gReI89RJn0-RQ",
-    mainText: "Paris",
-    secondaryText: "KY, USA",
-  },
-];
-
 export default function usePlaceAutocomplete() {
   const getPlaces = async (value: string) => {
     if (!value.trim()) {
