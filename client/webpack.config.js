ecb7d9ada04d6d69cb86662e5d4bef7101ef5eae
Oscar <orojas12@outlook.com>
2023-03-04 11:18:14 -0700

    bugfix: webpack dev server error 504

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,7 @@ const config = {
     historyApiFallback: true,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://127.0.0.1:8080",
       },
     },
   },
