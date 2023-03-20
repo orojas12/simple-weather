c33e32f256731764b1e90ce7e4576e40427c7791
Oscar Rojas <orojas12@outlook.com>
2023-03-20 13:10:50 -0600

    add map page

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,6 @@
 import ErrorPage from "./ErrorPage";
 import DashboardPage from "./DashboardPage/DashboardPage";
 import LocationsPage from "./LocationsPage/LocationsPage";
+import MapPage from "./MapPage/MapPage";
 
-export { ErrorPage, DashboardPage, LocationsPage };
+export { ErrorPage, DashboardPage, LocationsPage, MapPage };
