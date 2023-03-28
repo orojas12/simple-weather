f9147734f0f7f739bfbc290c9cea454d8a79d188
Oscar Rojas <orojas12@outlook.com>
2023-03-28 10:28:10 -0600

    remove invalid imports/exports

--- client/src/hooks/index.ts
+++ client/src/hooks/index.ts
@@ -1,28 +1,13 @@
-import { useLocation } from "./useLocation";
-import useWeather, {
-  WeatherContext,
-  WeatherData,
-  Weather,
-  WeatherCurrent,
-  WeatherDay,
-  WeatherHour,
-  WeatherAlert,
-} from "./useWeather";
+import useLocation from "./useLocation";
+import useWeather from "./useWeather";
 import usePlaceAutocomplete from "./usePlaceAutocomplete";
 import useGeocode from "./useGeocode";
 import useSettings from "./useSettings";
 
-export type { WeatherData };
 export {
   useLocation,
   useWeather,
   usePlaceAutocomplete,
   useGeocode,
   useSettings,
-  WeatherContext,
-  Weather,
-  WeatherCurrent,
-  WeatherDay,
-  WeatherHour,
-  WeatherAlert,
 };
