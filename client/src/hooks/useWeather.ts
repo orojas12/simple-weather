cc2d0b263bd8492b1e411456e4105ee3b8b4a386
Oscar Rojas <orojas12@outlook.com>
2023-03-28 10:29:17 -0600

    fix misnamed function

--- client/src/hooks/useWeather.ts
+++ client/src/hooks/useWeather.ts
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { ContextNotFoundError, WeatherContext } from "context";
 
-export default function useLocation() {
+export default function useWeather() {
   const weather = useContext(WeatherContext);
 
   if (!weather) throw new ContextNotFoundError("WeatherContext");
