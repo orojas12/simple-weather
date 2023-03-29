756ed795270453c72561f681cfd1decc8fa08490
Oscar Rojas <orojas12@outlook.com>
2023-03-29 12:46:43 -0600

    add module aliases to jest configuration

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,10 +1,18 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/js-with-ts",
   testEnvironment: "jsdom",
+  transformIgnorePatterns: ["node_modules/(?!nanoid/.*)"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+    "^@context(.*)$": "<rootDir>/src/context$1",
+    "^@components(.*)$": "<rootDir>/src/components$1",
+    "^@hooks(.*)$": "<rootDir>/src/hooks$1",
+    "^@icons(.*)$": "<rootDir>/src/icons$1",
+    "^@lib(.*)$": "<rootDir>/src/lib$1",
+    "^@pages(.*)$": "<rootDir>/src/pages$1",
   },
 };
