6374dfccac69a1b7376cbd090b24bda652f6c20d
Oscar Rojas <orojas12@outlook.com>
2023-04-03 18:20:18 -0600

    add axios

---
+++ client/src/lib/axios.ts
@@ -0,0 +1,21 @@
+import Axios, { AxiosError } from "axios";
+import { API_URL } from "@config";
+import { ApiErrorBody } from "@types";
+import { useNotifications } from "@hooks";
+
+export const axios = Axios.create({
+  baseURL: API_URL,
+});
+
+axios.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error: AxiosError<ApiErrorBody>) => {
+    const defaultMessage = `Error ${error.response?.status}`;
+    useNotifications().addNotification({
+      type: "error",
+      message: error.response?.data?.message || defaultMessage,
+    });
+  }
+);
