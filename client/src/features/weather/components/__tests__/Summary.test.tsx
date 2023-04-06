617fe2e87e7d26bacafb884a35f8559e32b80475
Oscar Rojas <orojas12@outlook.com>
2023-04-06 16:51:51 -0600

    add tests

--- client/src/features/weather/components/__tests__/Summary.test.tsx
+++ client/src/features/weather/components/__tests__/Summary.test.tsx
@@ -16,8 +16,21 @@ describe("Summary component", () => {
     const { user } = setup(
       <Summary day={0} weather={{} as any} location={{} as any} />
     );
-
     expect(screen.getByTestId("current-summary")).toBeInTheDocument();
     expect(screen.queryByTestId("day-summary")).toBe(null);
+  });
+
+  test("show current/today toggle if day is today", () => {
+    const { user } = setup(
+      <Summary day={0} weather={{} as any} location={{} as any} />
+    );
+    expect(screen.getByRole("group")).toBeInTheDocument();
+  });
+
+  test("do not show current/today toggle if day is not today", () => {
+    const { user } = setup(
+      <Summary day={1} weather={{} as any} location={{} as any} />
+    );
+    expect(screen.queryByRole("group")).toBe(null);
   });
 });
