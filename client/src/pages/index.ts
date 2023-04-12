0c66ee16cf29ac33a583097d55ca6d5918318440
Oscar Rojas <orojas12@outlook.com>
2023-04-12 10:20:18 -0600

    delete dashboardpage folder

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,7 +1,6 @@
 import ErrorPage from "./ErrorPage";
-import DashboardPage from "./DashboardPage/DashboardPage";
 import LocationsPage from "./LocationsPage/LocationsPage";
 import MapPage from "./MapPage/MapPage";
 import SettingsPage from "./SettingsPage/SettingsPage";
 
-export { ErrorPage, DashboardPage, LocationsPage, MapPage, SettingsPage };
+export { ErrorPage, LocationsPage, MapPage, SettingsPage };
