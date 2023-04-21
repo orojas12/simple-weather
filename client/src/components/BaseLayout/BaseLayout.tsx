83dd048b93b3607dfa07eadab336ef42d021cec3
Oscar Rojas <orojas12@outlook.com>
2023-04-21 17:46:12 -0600

    change classname

--- client/src/components/BaseLayout/BaseLayout.tsx
+++ client/src/components/BaseLayout/BaseLayout.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import { Navbar } from "@/components";
 
 export default function BaseLayout(props: { children: React.ReactNode }) {
-  return (
-    <div className="BaseLayout">
-      <Navbar />
-      <div className="content">{props.children}</div>
-    </div>
-  );
+  return <div className="base-layout">{props.children}</div>;
 }
