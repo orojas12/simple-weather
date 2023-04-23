88abfebf596770a46ed8451e5166446a86023406
Oscar Rojas <orojas12@outlook.com>
2023-04-23 16:38:14 -0600

    add production configuration

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,1 +1,1 @@
-export const API_URL = "http://0.0.0.0:8080/api";
+export const API_URL = process.env.API_URL;
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
 const path = require("path");
+const Dotenv = require("dotenv-webpack");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
@@ -24,7 +25,7 @@ const config = {
     historyApiFallback: true,
   },
   devtool: "inline-source-map",
-  plugins: [],
+  plugins: [new Dotenv()],
   module: {
     rules: [
       {
